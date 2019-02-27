947524eabd40019d7416ecd5fc3e5731f495e1e1
Monastyrev Ivan <ikloster@yandex.ru>
2019-02-27 16:52:52 +0300

    Fix test invalidate

--- test/index.test.js
+++ test/index.test.js
@@ -64,7 +64,8 @@ describe('jwt redis', () => {
 
     const value = await jwtr.get(token)
     expect(value).not.toBeNull()
-    expect('' + JWTR.decode(token).exp).toBe(value.exp)
+    const exp = value.exp * 1000
+    expect('' + JWTR.decode(token).exp).toBe(exp)
   })
 
   test('validate', async () => {
